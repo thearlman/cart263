428b8e17efbd76e078bd2397137902f717f68022
thearlman <asaperlman@gmail.com>
2020-01-14 13:35:51 -0500

    added p5 template

--- p5Template/js/script.js
+++ p5Template/js/script.js
@@ -9,3 +9,9 @@ This is a template. Fill in the title, author, and this description
 to match your project! Write JavaScript to do amazing things below!
 
 *********************************************************************/
+
+function setup(){
+  console.log("hello");
+  const x = 1;
+  x = 2;
+}
