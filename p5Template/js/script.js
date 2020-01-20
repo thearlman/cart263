a4ed8a43f190c6ca74e40d83a51d3c51663159b1
thearlman <asaperlman@gmail.com>
2020-01-20 11:53:33 -0500

    reset template file

--- p5Template/js/script.js
+++ p5Template/js/script.js
@@ -1,17 +1,1 @@
-"use strict";
-
-/********************************************************************
-
-Title of Project
-Author Name
-
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
-
-*********************************************************************/
-
-function setup(){
-  console.log("hello");
-  const x = 1;
-  x = 2;
-}
+//~~~~~~~~~~put javaScript here~~~~~~~~~~~
