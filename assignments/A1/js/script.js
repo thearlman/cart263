a982b67a10c09c4430a71a43077a8fd8e80418fb
thearlman <asaperlman@gmail.com>
2020-01-20 16:56:36 -0500

    A1: added GUI for the color change

--- assignments/A1/js/script.js
+++ assignments/A1/js/script.js
@@ -1,38 +1,56 @@
 "use strict";
+
+//the current color user is painting with
+let currentColor = 'rainbow';
+
+//~~~~~~~~$()~~~~~~~~~
+//
+//returns an element on the pagee, according to the id it was fed
+//this means that: $("someId") can be used in place of the whole string
+function $(id) {
+  return document.getElementById(id);
+}
+
+//key-values for all the colors available.
+let colors = {
+  rainbow : `rgb(${randomColorPicker()}, ${randomColorPicker()}, ${randomColorPicker()})`,
+  red : `rgb(255,0,0)`,
+  green : `rgb(0,255,0)`,
+  blue : 'rgb(0,0,255)',
+  black : 'rgb(0,0,0)',
+  eraser : 'rgb(255,255,255)'
+};
+
+//~~~~~colorPicker()~~~~~~~
+//
+//picks a random number between 0 and 255 and returns it.
+function randomColorPicker() {
+  return Math.floor(Math.random() * 255);
+}
+
+//~~~~~~setColor()~~~~~~
+//
+//sets the color of the pixel it was passed
+function setColor(pixel, color) {
+  pixel.style.background = color;
+  console.log(color);
+}
+
+//sets the fade time of the pixel it was passed
+function setFade(pixel) {
+  setTimeout(() => {
+    pixel.style.background = "rgb(255,255,255)"
+  }, 500);
+}
+
 //wait for the window object to load before doing anything
 window.onload = () => {
-
-  //~~~~~~~~$()~~~~~~~~~
-  //
-  //returns an element on the pagee, according to the id it was fed
-  //this means that: $("someId") can be used in place of the whole string
-  function $(id) {
-    return document.getElementById(id);
-  }
-
-  //key-values for all the colors available.
-  let colors = {
-    rainbow : `rgb(${colorPicker()}, ${colorPicker()}, ${colorPicker()})`,
-    red : `rgb(255,0,0)`,
-    green : `rgb(0,255,0)`,
-    blue : 'rgb(0,0,255)'
-  };
-  let currentColor = 'green';
-
-  //~~~~~colorPicker()~~~~~~~
-  //
-  //picks a random number between 0 and 255 and returns it.
-  function colorPicker() {
-    return Math.floor(Math.random() * 255);
-  }
-
   //~~~~~makeCanvas()~~~~~~~
   //
   //mmakes a canvas out of a bunch of div "pixels".
   function makeCanvas() {
     let canvas = $("canvas");
     canvas.draggable = false;
-    console.log("hello" + canvas.offsetWidth);
     //classic for loop up to 1000
     for (let i = 0; i < 10000; i++) {
       //create a div and call it "pixel"
@@ -60,21 +78,6 @@ window.onload = () => {
       }
     }
   }
-
-  //~~~~~~setColor()~~~~~~
-  //
-  //sets the color of the pixel it was passed
-  function setColor(pixel, color) {
-    pixel.style.background = color;
-  }
-
-  //sets the fade time of the pixel it was passed
-  function setFade(pixel) {
-    setTimeout(() => {
-      pixel.style.background = "rgb(255,255,255)"
-    }, 500);
-  }
-
   // lets make a canvas!
   makeCanvas();
 
